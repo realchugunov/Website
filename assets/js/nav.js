80e53a471d75a7696048a574397803341e260a62
realchugunov <realchugunov@hotmail.com>
2023-09-06 15:35:55 +0300

    Team Members section is done + Division sectionWIP

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,20 +1,8 @@
-window.addEventListener('DOMContentLoaded', () => {
-
-	const observer = new IntersectionObserver(entries => {
-		entries.forEach(entry => {
-			const id = entry.target.getAttribute('id');
-			if (entry.intersectionRatio > 0) {
-				document.querySelector(`header`).parentElement.classList.add('active');
-			} else {
-				document.querySelector(`header`).parentElement.classList.remove('active');
-			}
-		});
-	});
-
-	// Track all sections that have an `id` applied
-	document.querySelectorAll('section[id]').forEach((section) => {
-		observer.observe(section);
-	});
-	
+window.addEventListener("scroll", function() {
+    const navbar = document.querySelector(".header");
+    if (window.scrollY > 0) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
 });
-
