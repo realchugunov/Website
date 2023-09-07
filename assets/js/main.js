fe5cca0337f3ac024e99976d314933a4c0bf05d5
realchugunov <realchugunov@hotmail.com>
2023-09-07 13:31:02 +0300

    "Our Team" Section done. Division img changed

--- assets/js/main.js
+++ assets/js/main.js
@@ -758,29 +758,6 @@
 
 	} // initKenburns
 
-
-
-/* --------------------------------------------------
-	Coming Soon - Countdown
--------------------------------------------------- */
-
-	function initCountdown () {
-
-		var hasCountdown = $('#cs-timer').hasClass('cs-timer');
-
-		if (hasCountdown) {
-
-			// Add end date here (current: 2017/01/01) from witch the timer will countdown.
-			$('#cs-timer').countdown('2017/01/01', function(event) {
-			    $(this).html(event.strftime('<div class="item"><span class="nbr-timer">%D</span><span class="title-timer">Days<span></div><div class="item"><span class="nbr-timer">%H</span><span class="title-timer">Hours<span></div><div class="item"><span class="nbr-timer">%M</span><span class="title-timer">Minutes<span></div><div class="item"><span class="nbr-timer">%S</span><span class="title-timer">Seconds<span></div>'));
-			  });
-
-		}
-
-	}
-
-
-
 /* --------------------------------------------------
 	Shop Price Filter - (range slider)
 -------------------------------------------------- */
