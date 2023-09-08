67a94495ebbf7c42d5d963f8b067cd528be4c28f
realchugunov <realchugunov@hotmail.com>
2023-09-08 23:13:21 +0300

    fixed navbar

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,8 +1,10 @@
-window.addEventListener("scroll", function() {
-    const navbar = document.querySelector(".header");
-    if (window.scrollY > 1000) {
-        navbar.classList.add("scrolled");
+window.addEventListener("scroll", function () {
+    const navbar = document.getElementById("header");
+    const scrollPosition = window.scrollY;
+
+    if (scrollPosition > 100) { // Adjust this value as needed
+        navbar.classList.add("sticky", "white"); // Add both classes
     } else {
-        navbar.classList.remove("scrolled");
+        navbar.classList.remove("sticky", "white"); // Remove both classes
     }
 });
