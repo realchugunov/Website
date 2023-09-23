e9780f359caf3f8a1add681daeb36154a985455b
realchugunov <realchugunov@hotmail.com>
2023-09-23 14:47:25 +0300

    Partner slide moves faster, division got centered.

--- assets/js/main.js
+++ assets/js/main.js
@@ -385,7 +385,7 @@
 		// Brands/Clients Slider
 		$('.clients-slider').slick({
 			autoplay: true,
-			autoplaySpeed: 4000,
+			autoplaySpeed: 1000,
 			slidesToShow: 5,
 			slidesToScroll: 1,
 			dots: false,
